2697ac73112406ee273f6fc01c185a9223ff7772
jbjzeehad <ilamta.97@gmail.com>
2024-03-02 18:06:19 +0600

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+    return (
+        <div className="m-56">
+            <p className="font-bold text-xl text-center text-red-900">Project Task</p>
+        </div>
+    );
+};
+
+export default App;
