e8adb725dc99fe979fa1600ee85b299079f11e79
jbjzeehad <ilamta.97@gmail.com>
2024-03-04 21:30:47 +0600

    navbar

--- src/Sections/ImageSection.jsx
+++ src/Sections/ImageSection.jsx
@@ -3,7 +3,7 @@
 const ImageSection = () => {
     return (
         <div className="mx-[211px] my-[60px]">
-            <img src="../../public/section.png" alt="" />
+            <img src="/section.png" alt="" />
         </div>
     );
 };
